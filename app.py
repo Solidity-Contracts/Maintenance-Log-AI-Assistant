e7705bb75616ec6ee9e348c7189ef49d14ec0c80
Solidity contracts <54622533+Solidity-Contracts@users.noreply.github.com>
2024-10-08 12:02:55 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,16 +9,18 @@ import json
 
 from config import OPENAI_API_KEY
 
+print (OPENAI_API_KEY)
 
-if OPENAI_API_KEY:
+api_key = OPENAI_API_KEY
+if api_key:
     st.success("API Key loaded successfully!")
 else:
     st.error("API key is not set. Please check your .env file.")
 
 
 # Access the API key from the environment variable
-#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 # Set up Web3 connection to Sepolia using Infura
 infura_url = "https://sepolia.infura.io/v3/4aa0e165e1a14e7faf087f9dc54b183b"
